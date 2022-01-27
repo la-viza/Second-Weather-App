f242d1cd182b0531d5c42e76c4ca5b53e3d858c6
la-viza <97736137+la-viza@users.noreply.github.com>
2022-01-27 13:12:53 +0300

    Integrated API and real time update

---
+++ app.js/app.js
@@ -0,0 +1,56 @@
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature (response) {
+console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windElement=document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+
+
+
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML=response.data.weather[0].description;
+humidityElement.innerHTML=`Humidity: ${response.data.main.humidity}%`;
+windElement.innerHTML=`Wind: ${Math.round(response.data.wind.speed)} km/h`;
+dateElement.innerHTML= formatDate(response.data.dt * 1000);
+
+
+
+}
+
+
+
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
